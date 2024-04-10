fc4da05474469dd37d04b68ddcbb500d4153c819
Tulonga Milapo <brookstulonga@gmail.com>
2024-04-10 17:21:13 +0200

    Landing Page

---
+++ resources/views/products/create.blade.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Create</title>
+</head>
+<body>
+
+<form method="post" action"{{route('Products.store')}}">
+    <div>
+        <label for="">Name</label>
+        <input type="string" placeholder="name" />
+    </div>
+
+    <br>
+
+    <div>
+        <label for="">Description</label>
+        <input type="text" placeholder="description" />
+    </div>
+
+    <br>
+
+    <div>
+        <label for="">Price</label>
+        <input type="decimal" placeholder="price" />
+    </div>
+
+    <br>
+
+    <div>
+        <label for="">Quantity</label>
+        <input type="integer" placeholder="qty" />
+    </div>
+
+    <br>
+
+    <div>
+        <label for="">Category</label>
+        <input type="string" placeholder="category" />
+    </div>
+
+    <br>
+
+    <div>
+        <input type="submit" value="Save A New Product">
+    </div>
+   
+</form>
+    
+</body>
+</html>
+
+<!--    'product_name',
+        'description',
+        'price',
+        'stock_quantity',
+        'category_id' -->
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,39 @@
 <?php
 
-use App\Http\Controllers\ProductsController;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+use App\Http\Controllers\ProductsController;
+
+Route::get('/', function () {
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
+});
+
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+
 Route::get('/products', [ProductsController::class, 'index'])->name('Products.index');
+
+Route::get('/products/create', [ProductsController::class, 'create'])->name('Products.create');
+
+Route::post('/products', [ProductsController::class, 'store'])->name('Products.store');
+
+require __DIR__.'/auth.php';
